d12f3bdc16c175da49f1e7668e472fd7bde07538
Sigit Haryoyudanto <hello@sigit.co.uk>
2023-09-11 08:22:22 +0000

    first commit

---
+++ 1155drop/const/yourDetails.ts
@@ -0,0 +1,5 @@
+// Replace this with your contract address
+export const myEditionDropContractAddress: string = "0xf82F9B02B6Ea144d1E08944dbbdECc610d54d343";
+
+// Replace this with your token id
+export const tokenId: string = "0";
